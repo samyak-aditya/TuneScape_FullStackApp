1ef4c5e6cdfd28be3b18ef8156a2efe503cae594
KNIGHTW4LKER <samyakaditya0000@gmail.com>
2023-10-07 01:28:10 +0530

    mocha testing setup

--- tests/api-tests.js
+++ tests/api-tests.js
@@ -6,39 +6,50 @@ before(async () => {
   // Import your Express app using dynamic import
   const expressApp = await import('../server/routes/routes.js');
   app = expressApp.default; // Assign the app instance to the app variable
+  
 }); // Import your Express app instance
 const expect = chai.expect;
 
 chai.use(chaiHttp);
 
 describe('Spotify API Endpoints', () => {
+  const host = 'http://localhost'; // Specify the host
+  const port = 5000; // Specify the port
+
   it('should get discover data', async () => {
-    const res = await chai.request(app).get('/discover');
-    console.log(" =========>>>>>" +res);
-    //expect(res).to.have.status(200);
-    //expect(res.body).to.be.an('json');
-    
-    // Add more assertions for the specific response data as needed
+    const res = await chai.request(`${host}:${port}`).get('/discover');
+    // Add assertions for the response data as needed
+    expect(res).to.have.status(200);
+
+  // Assert that the response data is an object
+  expect(res.body).to.be.an('object');
   });
 
-
-it('should get artist data', (done) => {
+  it('should get artist data', (done) => {
     chai
-      .request(app)
+      .request(`${host}:${port}`)
       .get('/artists')
       .end((err, res) => {
-        console.log(" =========>>>>>" +res);
-        //expect(res).to.have.status(200);
-        //expect(res.body).to.be.an('object'); // Assuming artists data is an object
-        // Add more assertions for the specific response data as needed
-        done();
-        console.log(" ====*****=>>>>>" +res)
+        if (err) {
+          done(err); // Call done with the error if there is one
+          return;
+        }
+        
+        // Add your assertions for the response here
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('object'); // Assuming artists data is an object
+  
+        done(); // Call done to indicate the test is complete
       });
   });
 
- /* it('should get track data', (done) => {
+
+  
+  
+
+  it('should get track data', (done) => {
     chai
-      .request(app)
+      .request(`${host}:${port}`)
       .get('/tracks')
       .end((err, res) => {
         expect(res).to.have.status(200);
@@ -50,7 +61,7 @@ it('should get artist data', (done) => {
 
   it('should get playlist data', (done) => {
     chai
-      .request(app)
+      .request(`${host}:${port}`)
       .get('/playlists')
       .end((err, res) => {
         expect(res).to.have.status(200);
@@ -62,7 +73,7 @@ it('should get artist data', (done) => {
 
   it('should get genre data', (done) => {
     chai
-      .request(app)
+      .request(`${host}:${port}`)
       .get('/genre')
       .end((err, res) => {
         expect(res).to.have.status(200);
@@ -74,7 +85,7 @@ it('should get artist data', (done) => {
 
   it('should get podcast data', (done) => {
     chai
-      .request(app)
+      .request(`${host}:${port}`)
       .get('/podcast')
       .end((err, res) => {
         expect(res).to.have.status(200);
@@ -82,5 +93,5 @@ it('should get artist data', (done) => {
         // Add more assertions for the specific response data as needed
         done();
       });
-  });*/
+  });
 });
